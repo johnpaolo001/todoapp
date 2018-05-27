7ce71db38648f6088c535e8d79f2ed872cf7ba32
John Paolo Paulino <paolo716@gmail.com>
2018-05-27 21:55:59 +0800

    creaing todos from UI

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,4 +3,23 @@ class TodosController < ApplicationController
   def index
 
   end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to todo_path(@todo)
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  private
+
+    def todo_params
+      params.require(:todo).permit(:name, :description)
+    end
 end
